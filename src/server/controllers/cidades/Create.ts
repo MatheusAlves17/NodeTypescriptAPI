a9cb3fcb9d2a5ad7ca43d073ad45a222d4d99a4e
Matheus Alves <matheusfrancisco0330@gmail.com>
2022-12-19 18:14:14 -0300

    feat: criando validações das requisições

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,9 +1,26 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import * as yup from 'yup';
 
 interface ICidade{
-    nome: String;
+    nome: string;
 }
 
-export const Create = (req: Request<{}, {}, ICidade>, res: Response) => {
+const bodyValidation: yup.SchemaOf<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3)
+})
+
+export const Create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    let validateDate: ICidade | undefined = undefined;
+    try {
+        validateDate = await bodyValidation.validate(req.body)
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        return res.json({
+            errors: {
+                default: yupError.message
+            }
+        })
+    }
+
     return res.send(`Create ${req.body.nome}`);
 }
