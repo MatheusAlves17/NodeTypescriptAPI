98275cc0c003118191c592da22264706e33981ba
Matheus Alves <matheusfrancisco0330@gmail.com>
2022-12-19 16:49:09 -0300

    feat: variáveis de ambiente e build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from "./server/Server";
 
-server.listen(3333, () => {
-  console.log("Servidor tá on!");
+server.listen(process.env.PORT || 3333, () => {
+  console.log(`Servidor tá on na porta ${process.env.PORT || 3333}!`);
 });
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import express from 'express';
 import { router } from './routes';
+
 
 const server = express();
 
